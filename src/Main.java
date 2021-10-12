147c346f404f12ae526938b4f81ab8bf78ef15bc
Viki <masalikavika@yandex.ru>
2021-10-12 18:19:20 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1000;  //Цена билета
+        int rubForMile = 20;     //Рублей за 1 милю
+        int quantityMile;        //количество начисленных мили
+
+        quantityMile = priceTicket/20;
+
+        System.out.println("Всего мили:" + quantityMile);
+
+    }
+}
