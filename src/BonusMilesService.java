160928b949acaa61eb79f6dd5594469167bcd520
Viki <masalikavika@yandex.ru>
2021-10-19 20:47:07 +0300

    commit new class

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate (int priceTicket){
+        int quantityMile = priceTicket/20;
+        return quantityMile;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int priceTicket = 1000;  //Цена билета
-        int rubForMile = 20;     //Рублей за 1 милю
         int quantityMile;        //количество начисленных мили
 
-        quantityMile = priceTicket/20;
+        BonusMilesService service = new BonusMilesService();
+        quantityMile = service.calculate(priceTicket);
 
         System.out.println("Всего мили:" + quantityMile);
 
